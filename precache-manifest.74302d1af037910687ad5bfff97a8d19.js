b91b7ba4c20ddd490c8e9e582b9c66c3cae30c4a
Nandini Mehta <nandu49200@gmail.com>
2020-07-17 16:05:45 +0530

    Updates

---
+++ precache-manifest.74302d1af037910687ad5bfff97a8d19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033d31f704cb92cdf32c6b44392caddc",
+    "url": "/Corona-virus-tracker/index.html"
+  },
+  {
+    "revision": "8c54b7100e37bc6ffdb8",
+    "url": "/Corona-virus-tracker/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "4f7aa40b396af33bf1c5",
+    "url": "/Corona-virus-tracker/static/js/2.88ee71bc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-virus-tracker/static/js/2.88ee71bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c54b7100e37bc6ffdb8",
+    "url": "/Corona-virus-tracker/static/js/main.594e5cac.chunk.js"
+  },
+  {
+    "revision": "cc1e8e6491c676b9fb30",
+    "url": "/Corona-virus-tracker/static/js/runtime-main.f1617454.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-virus-tracker/static/media/image.d7265326.png"
+  }
+]);
